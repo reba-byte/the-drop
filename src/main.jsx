a5c12d20a1e7c356c6e594c3c6e6032003bd90e1
reba-byte <rhenderson@smartpathed.com>
2026-01-17 21:37:15 -0600

    updateonesignalcdn

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,6 @@ import { AuthProvider } from './context/AuthContext'
 import { GroupProvider } from './context/GroupContext'
 import './index.css'
 import App from './App.jsx'
-import OneSignal from 'react-onesignal';
-
-// Initialize OneSignal
-async function initOneSignal() {
-  try {
-    await OneSignal.init({
-      appId: "d1ed5b32-2eed-44b7-9b70-747de55a3573",
-      allowLocalhostAsSecureOrigin: true,
-    });
-    console.log('OneSignal initialized')
-  } catch (error) {
-    console.error('OneSignal init error:', error)
-  }
-}
-
-initOneSignal()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
